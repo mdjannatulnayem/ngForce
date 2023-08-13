583fa9184baeaeaa3bdcc2800db3015ccfad6d53
nayem <nayem@SNIFFER-JR>
2023-08-13 23:38:13 +0600

    changed to .py

---
+++ pyCommand/main.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt,keyboard,time,requests
+
+
+def get_data_from_api(api_route):
+    try:
+        response = requests.get(api_route)
+        response.raise_for_status() 
+        # Raise an exception for 4xx and 5xx status codes!
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print("Error fetching data:", e)
+        return None
+
+
+api_route = input("Enter the API route: ")
+
+count = 1
+x_values = []
+y_values = []
+
+while True:
+    launch = get_data_from_api(api_route+'/ngforce/ignition')
+    if (keyboard.is_pressed('L') or keyboard.is_pressed('l')):
+        get_data_from_api(api_route+'/ngforce/ignition/1')
+    if launch:
+        data = get_data_from_api(api_route+'/ngforce/engine/last')
+        if data is not None:
+            x_values.append(count)
+            y_values.append(data)
+            count += 1
+            print("Thrust: ", data)
+        else:
+            print("No entry has been made!")
+    if (keyboard.is_pressed('Q') or keyboard.is_pressed('q')):
+        plt.plot(x_values,y_values)
+        plt.show()
+        print("// Terminated!")
+        break
